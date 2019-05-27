01004a4a0db779eca503a71606cebe96345ca7c3
Leif Denby <leif@denby.eu>
2019-05-27 11:39:45 +0100

    TileType defined in architecture module

--- convml_tt/__init__.py
+++ convml_tt/__init__.py
@@ -4,11 +4,3 @@ import fastai
 from .architectures.triplet_trainer import NPMultiImageList
 
 from . import interpretation
-
-
-class TileType:
-    """
-    Simple enum for mapping into triplet array """
-    ANCHOR = 0
-    NEIGHBOUR = 1
-    DISTANT = 2
