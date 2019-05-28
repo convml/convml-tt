e5331257433d5731fcb4f2d90953b337fe0dc5a3
DENBY Leif <leif@denby.eu>
2019-05-28 18:56:53 +0200

    Bump version to v0.2.0
    With the fixes to normalisation in the triplet trainer I'm tagging a new
    version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='convml_tt',
     packages=find_packages(exclude=['contrib', 'tests', 'docs']),
-    version='0.1.0',
+    version='0.2.0',
     description='Neural Network based study of convective organisation',
     author='Leif Denby',
     author_email='leifdenby@gmail.com',
