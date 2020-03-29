d2a28abed410809e12278014ef9b65273131eaac
Leif Denby <leif@denby.eu>
2020-03-29 11:22:43 +0100

    add cmd for fetch data

---
+++ convml_tt/data/dataset/fetch_source_data.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from . import TripletDataset
+
+
+
+if __name__ == "__main__":
+    import argparse
+
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--dataset-path', default=".")
+
+    args = argparser.parse_args()
+
+    dataset = TripletDataset.load(args.path)
+    dataset.fetch_source_data()
