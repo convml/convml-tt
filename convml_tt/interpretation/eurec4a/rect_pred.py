9c98e348221ec0e94f969390edad893f4d07a9e2
Leif Denby <leif@denby.eu>
2020-04-20 12:46:10 +0100

    attempt quickfix 2

--- convml_tt/interpretation/eurec4a/rect_pred.py
+++ convml_tt/interpretation/eurec4a/rect_pred.py
@@ -134,7 +134,7 @@ class DatasetImagePredictionMapData(ImagePredictionMapData):
 
     @property
     def src_data_path(self):
-        return self.input().input().output().fn
+        return self.requires().input().output().fn
 
     def output(self):
         fn = "{}.embeddings.{}_step.nc".format(
