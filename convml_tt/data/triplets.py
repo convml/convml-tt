231a6a2aa809a197778e9fef4397d25b2324b9e3
Leif Denby <leif@denby.eu>
2021-02-18 12:07:29 +0000

    remove unused import

--- convml_tt/data/triplets.py
+++ convml_tt/data/triplets.py
@@ -4,7 +4,6 @@ Utility class for loading tile definitions from existing datasets
 
 import yaml
 from tqdm import tqdm
-import satdata
 
 from ..architectures.triplet_trainer import TileType
 from .sources.satellite.tiler import Tile
@@ -19,12 +18,13 @@ class TripletTile(Tile):
         self.tile_id = tile_id
         self.data_path = data_path
 
+
 def load_tile_definitions(triplets, tile_type=TileType.ANCHOR):
     def _load_tile_from_triplet(triplet):
         fn_triplet_meta = triplets.TRIPLET_META_FILENAME_FORMAT.format(
             triplet_id=triplet.id
         )
-        path_triplet_meta = triplet.src_path/fn_triplet_meta
+        path_triplet_meta = triplet.src_path / fn_triplet_meta
 
         # the things stored in the triplet are actually the RGB images, these
         # will be handy for plotting later
@@ -32,21 +32,23 @@ def load_tile_definitions(triplets, tile_type=TileType.ANCHOR):
 
         meta = yaml.load(open(path_triplet_meta), Loader=yaml.SafeLoader)
         if tile_type in [TileType.ANCHOR, TileType.NEIGHBOR]:
-            meta_group = meta['target']
+            meta_group = meta["target"]
             tile_meta = meta_group[tile_type.name.lower()]
-            source_files = meta_group['source_files']
+            source_files = meta_group["source_files"]
         elif tile_type == TileType.DISTANT:
-            tile_meta = meta['distant']['loc']
-            source_files = meta['distant']['source_files']
+            tile_meta = meta["distant"]["loc"]
+            source_files = meta["distant"]["source_files"]
         else:
             raise NotImplementedError
 
         tile = TripletTile(
             rgb_img=rgb_img,
             meta=dict(rgb_source_files=source_files),
-            lat0=tile_meta['lat'], lon0=tile_meta['lon'],
-            size=tile_meta['size'],
-            tile_id=triplet.id, data_path=triplets.path.absolute()
+            lat0=tile_meta["lat"],
+            lon0=tile_meta["lon"],
+            size=tile_meta["size"],
+            tile_id=triplet.id,
+            data_path=triplets.path.absolute(),
         )
 
         return tile
