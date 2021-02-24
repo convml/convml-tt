e3e1d9e4655d64cefb944b5321595af333858815
Leif Denby <leif@denby.eu>
2021-02-24 01:38:02 +0000

    re-add versioneer

--- convml_tt/__init__.py
+++ convml_tt/__init__.py
@@ -4,3 +4,7 @@ import fastai
 from .architectures.triplet_trainer import NPMultiImageList
 
 from . import interpretation
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
