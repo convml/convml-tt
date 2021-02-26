f84bde26bc68eaeb0c28341c554462b1a0eab0eb
Leif Denby <leif@denby.eu>
2021-02-26 17:17:09 +0000

    add utility to extract pytorch model
    to avoid dependency on fastai in future this utility makes it possible to strip a fastai model of all fastai specific modules (the required once are duplicated inside convml_tt)

---
+++ convml_tt/external/extract_model.py
@@ -0,0 +1,49 @@
+# coding: utf-8
+"""
+Utility to strip pytorch-only model from fastai v1 `Learner` so the weights can
+be unpickled and we can create a pytorch model from the same weights without
+using fastai
+"""
+
+from torch import nn
+import torch
+from fastai.basic_train import load_learner
+
+
+def get_pytorch_model(model):
+    # the torch model is contained inside the fastai one as `model`
+    tmodel = model.model
+
+    # the head contains fastai-specific layers tha we can't unpickle unless
+    # fastai is installed
+    head = tmodel[-1]
+
+    new_head_layers = []
+    for n in range(len(head)):
+        layer = head[n]
+        if type(layer).__module__.startswith("fastai"):
+            # drop these layers, we'll have to remember to add them in
+            # ourselves
+            pass
+        else:
+            new_head_layers.append(layer)
+    new_head = nn.Sequential(*new_head_layers)
+
+    return nn.Sequential(*tmodel[:-1], new_head)
+
+
+def main(fastai_model_filename):
+    torch_model_filename = fastai_model_filename.replace(".pkl", ".torch.pkl")
+    model = load_learner(".", fastai_model_filename)
+    tmodel = get_pytorch_model(model)
+    torch.save(tmodel, torch_model_filename)
+    print(f"pure-torch model saved to `{torch_model_filename}`")
+
+
+if __name__ == "__main__":
+    import argparse
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("fastai_model_filename")
+    args = argparser.parse_args()
+
+    main(args.fastai_model_filename)
