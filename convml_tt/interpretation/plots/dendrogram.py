4b754ac798998d7dcc41e35a2f508c333a0ff2da
Leif Denby <leif@denby.eu>
2021-07-23 11:47:18 +0200

    add "best triplets" method for dendrogram samples
    Use anchor-neighbor distance for selecting tiles to show in dendrogram

--- convml_tt/interpretation/plots/dendrogram.py
+++ convml_tt/interpretation/plots/dendrogram.py
@@ -8,7 +8,7 @@ import sklearn.decomposition
 from sklearn.preprocessing import StandardScaler
 from scipy.cluster import hierarchy as hc
 
-from ...data.dataset import ImageSingletDataset, TileType
+from ...data.dataset import ImageSingletDataset, TileType, ImageTripletDataset
 
 
 # In[13]:
@@ -37,37 +37,96 @@ def _fix_labels(ax, leaf_mapping, label_clusters=False):
     return new_labels
 
 
+def _get_tile_image(tile_dataset, i, tile_type):
+    if isinstance(tile_dataset, ImageTripletDataset):
+        tile_image = tile_dataset.get_image(
+            index=i, tile_type=TileType[tile_type.upper()]
+        )
+    elif isinstance(tile_dataset, ImageSingletDataset):
+        tile_image = tile_dataset.get_image(index=i)
+    else:
+        raise NotImplementedError(tile_dataset)
+    return tile_image
+
+
 def dendrogram(
     da_embeddings,
+    n_samples=10,
     n_clusters_max=14,
+    sampling_method="random",
     debug=False,
     ax=None,
-    n_samples=10,
     show_legend=False,
     label_clusters=False,
     return_clusters=False,
     color="black",
-    sampling_method="random",
     linkage_method="ward",
     **kwargs
 ):
     """
+    Create a dendrogram plot representing the clustering with the embedding
+    vectors `da_embddings` with sample tiles below each leaf node of the
+    dendrogram. Parameters:
+
+    n_clusters_max:
+        number of clusters to terminate the clustering at
+    n_samples:
+        number of tiles to show for each cluster
+    sampling_method:
+        method by which the sample tiles which are shown are selected.
+        `random`: select the tiles at random from each cluster.
+        `center_dist`: select the tiles closest (by Eucledian distance) to the
+            cluster centroid in the embedding space.
+        `best_triplets`: sort tiles from triplets for which the anchor-neighbor
+            distance is closest in the embedding space
+        `worst_triplets`: sort tiles from triplets for which the anchor-neighbor
+            distance is largest in the embedding space
+    show_legend:
+        add a colour-legend for the different cluster IDs
+    return_clusters:
+        return the associated clustering of tile IDs as a 1D numpy array
+    color:
+        change the colour of the dendrogram lines
+
     Additional kwargs will be passed to scipy.cluster.hierarchy.dendrogram
     """
 
-    tile_dataset = ImageSingletDataset(
-        data_dir=da_embeddings.data_dir,
-        tile_type=da_embeddings.tile_type,
-        stage=da_embeddings.stage,
-    )
+    tile_type = None
+    if "tile_type" in da_embeddings.coords:
+        embeddings_tts = set(da_embeddings.tile_type.values)
+        if set([tt.name.lower() for tt in TileType]) == embeddings_tts:
+            tile_dataset = ImageTripletDataset(
+                data_dir=da_embeddings.data_dir,
+                stage=da_embeddings.stage,
+            )
+        else:
+            raise NotImplementedError(embeddings_tts)
+        if not "tile_type" in kwargs:
+            raise Exception(
+                "You must provide the tile_type when plotting a dendrogram"
+                " from embeddings of triplets"
+            )
+        tile_type = kwargs.pop("tile_type")
+    else:
+        tile_dataset = ImageSingletDataset(
+            data_dir=da_embeddings.data_dir,
+            tile_type=da_embeddings.tile_type,
+            stage=da_embeddings.stage,
+        )
+        tile_type = da_embeddings.tile_type
 
     if ax is None:
         fig, ax = plt.subplots(figsize=(14, 3))
     else:
         fig = ax.figure
 
+    if "tile_type" in da_embeddings.coords:
+        da_clustering = da_embeddings.sel(tile_type=tile_type)
+    else:
+        da_clustering = da_embeddings
+
     Z = hc.linkage(
-        y=da_embeddings,
+        y=da_clustering,
         method=linkage_method,
     )
 
@@ -157,7 +216,7 @@ def dendrogram(
         y_offset += 0.2
 
     for lid, leaf_id in enumerate(ddata["leaves"]):
-        img_idxs_in_cluster = da_embeddings.tile_id.values[
+        img_idxs_in_cluster = da_clustering.tile_id.values[
             leaf_mapping == leaf_id
         ].astype(int)
         if sampling_method == "random":
@@ -168,13 +227,40 @@ def dendrogram(
             except ValueError:
                 img_idxs = img_idxs_in_cluster
         elif sampling_method == "center_dist":
-            emb_in_cluster = da_embeddings.sel(tile_id=img_idxs_in_cluster)
+            emb_in_cluster = da_clustering.sel(tile_id=img_idxs_in_cluster)
             d_emb = emb_in_cluster.mean(dim="tile_id") - emb_in_cluster
             center_dist = np.sqrt(d_emb ** 2.0).sum(dim="emb_dim")
             emb_in_cluster["dist_to_center"] = center_dist
             img_idxs = emb_in_cluster.sortby("dist_to_center").tile_id.values[
                 :n_samples
             ]
+        elif sampling_method in ["best_triplets", "worst_triplets"]:
+            if "tile_type" not in da_embeddings.coords:
+                raise Exception(
+                    "Selection method based on triplets can only be used when"
+                    " passing in embeddings for triplets"
+                )
+            da_emb_in_cluster = da_embeddings.sel(tile_id=img_idxs_in_cluster)
+            da_emb_dist = da_emb_in_cluster.sel(
+                tile_type="anchor"
+            ) - da_emb_in_cluster.sel(tile_type="neighbor")
+            near_dist = np.sqrt(da_emb_dist ** 2.0).sum(dim="emb_dim")
+            da_emb_in_cluster["near_dist"] = near_dist
+            img_idxs_all = da_emb_in_cluster.sortby("near_dist").tile_id.values
+            if sampling_method == "best_triplets":
+                img_idxs = img_idxs_all[:n_samples]
+            else:
+                img_idxs = img_idxs_all[::-1][:n_samples]
+        elif sampling_method == "worst_triplets":
+            da_emb_in_cluster = da_embeddings.sel(tile_id=img_idxs_in_cluster)
+            da_emb_dist = da_emb_in_cluster.sel(
+                tile_type="anchor"
+            ) - da_emb_in_cluster.sel(tile_type="neighbor")
+            near_dist = np.sqrt(da_emb_dist ** 2.0).sum(dim="emb_dim")
+            da_emb_in_cluster["near_dist"] = near_dist
+            img_idxs = da_emb_in_cluster.sortby(
+                "near_dist", reverse=True
+            ).tile_id.values[:n_samples]
         else:
             raise NotImplementedError(sampling_method)
 
@@ -190,7 +276,9 @@ def dendrogram(
             ax.scatter(*leaf_xy, marker="s", label=lid, s=100)
 
         for n, img_idx in enumerate(img_idxs):
-            img = tile_dataset.get_image(index=img_idx)
+            img = _get_tile_image(
+                tile_dataset=tile_dataset, i=img_idx, tile_type=tile_type
+            )
 
             ax1 = fig.add_axes(
                 [xp - 0.5 * size, yh - size * 1.1 * (n + y_offset), size, size]
