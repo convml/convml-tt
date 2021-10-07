425f78dc1fa882b826d52a731b415b105049e798
Leif Denby <leif@denby.eu>
2021-10-07 17:45:57 +0100

    refactor datasource example

--- convml_tt/data/sources/examples.py
+++ convml_tt/data/sources/examples.py
@@ -4,21 +4,21 @@ Example datasources for use with the triplet-trainer
 import enum
 from pathlib import Path
 
-from torchvision.datasources.utils import download_and_extract_archive
+from torchvision.datasets.utils import download_and_extract_archive
 
 _URL_ROOT = "http://homepages.see.leeds.ac.uk/~earlcd/ml-datasources"
 
 
-class ExampleData(enum.Enum):
+class ExampleDatasource(enum.Enum):
     EUREC4A_SMALL = "eurec4a-small"
 
 
 # datasources tar-balls with their md5 hash
 _checks = {}
-_checks[ExampleData.EUREC4A_SMALL] = "2e19801de21608968d107ac01ecf2a3b"
+_checks[ExampleDatasource.EUREC4A_SMALL] = "2e19801de21608968d107ac01ecf2a3b"
 
 
-def _fetch_example(item: ExampleData, data_dir="data/"):
+def _fetch_example(item: ExampleDatasource, data_dir="data/"):
     """
     Downloads example datasource and returns the path to it
     """
@@ -31,7 +31,7 @@ def _fetch_example(item: ExampleData, data_dir="data/"):
     return Path(data_dir)
 
 
-def fetch_example_datasource(datasource: ExampleData, data_dir="data/"):
+def fetch_example_datasource(datasource: ExampleDatasource, data_dir="data/"):
     """
     Downloads example datasource and returns the path to it
     """
@@ -45,11 +45,11 @@ def main(args=None):
     import argparse
 
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("datasource_name", choices=[ds.name for ds in ExampleData])
+    argparser.add_argument("datasource_name", choices=[ds.name for ds in ExampleDatasource])
     argparser.add_argument("--path", default="data/", type=Path)
     args = argparser.parse_args(args=args)
     datasource_path = fetch_example_datasource(
-        datasource=ExampleData[args.datasource_name], data_dir=args.path
+        datasource=ExampleDatasource[args.datasource_name], data_dir=args.path
     )
     print(f"Downloaded example datasource `{args.datasource_name}` to `{datasource_path}`")
 
