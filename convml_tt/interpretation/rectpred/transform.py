0c0c2601d8ac3152b4b8f78dfcb9810bc4ebdb28
Leif Denby <leif@denby.eu>
2021-11-24 10:47:18 +0000

    merge

--- convml_tt/interpretation/rectpred/transform.py
+++ convml_tt/interpretation/rectpred/transform.py
@@ -132,6 +132,11 @@ def apply_transform(
         s = ",".join([f"{k}={v}" for (k, v) in kwargs])
         da_transformed.attrs["transform_extra_args"] = s
 
+    # explicitly copy over coords that we want to retain
+    for c in ["image_path", "src_data_path"]:
+        if c in da:
+            da_transformed[c] = da[c]
+
     if return_model:
         return da_transformed, model
     else:
