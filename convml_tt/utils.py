630f278e32557f2cec57a753d4c84c6d6c1af56d
Leif Denby <leif@denby.eu>
2021-12-03 14:20:07 +0000

    wip on removing sliding_window func

--- convml_tt/utils.py
+++ convml_tt/utils.py
@@ -108,6 +108,20 @@ def get_embeddings(
     if hasattr(tile_dataset, "data_dir"):
         attrs["data_dir"] = str(Path(tile_dataset.data_dir).absolute())
 
+    if isinstance(tile_dataset, MovingWindowImageTilingDataset):
+        # "unstack" the 2D array with coords (tile_id, emb_dim) to have coords (i0,
+        # j0, emb_dim), where `i0` and `j0` represent the index of the pixel in the
+        # original image at the center of each tile
+        i_img_tile, j_img_tile = tile_dataset.index_to_img_ij(da_emb.tile_id.values)
+        i_img_tile_center = (i_img_tile + 0.5 * tile_dataset.nxt).astype(int)
+        j_img_tile_center = (j_img_tile + 0.5 * tile_dataset.nyt).astype(int)
+        da_emb["i0"] = ("tile_id"), i_img_tile_center
+        da_emb["j0"] = ("tile_id"), j_img_tile_center
+        da_emb = da_emb.set_index(tile_id=("i0", "j0")).unstack("tile_id")
+
+        da_emb.attrs["tile_nx"] = tile_dataset.nxt
+        da_emb.attrs["tile_ny"] = tile_dataset.nyt
+
     return xr.DataArray(embeddings, dims=dims, coords=coords, attrs=attrs)
 
 
@@ -116,32 +130,8 @@ def make_sliding_tile_model_predictions(
     tile_dataset: MovingWindowImageTilingDataset,
     prediction_batch_size=32,
 ):
-    """
-    Produce moving-window prediction array from at moving-window tiling dataset
-    with `model`.
-
-    NB: j-indexing is from "top_left" i.e. is likely in the opposite order to
-    what would be expected for y-axis of original image (positive being up)
-
-    i0 and j0 coordinates denote center of each prediction tile
-    """
-    da_emb = get_embeddings(
-        tile_dataset=tile_dataset,
-        model=model,
-        prediction_batch_size=prediction_batch_size,
+    raise NotImplementedError(
+        "`make_sliding_tile_model_predictions` has been removed and its functionality"
+        " added to `utils.get_embeddings`. Please change to use `utils.get_embeddings`"
+        " instead."
     )
-
-    # "unstack" the 2D array with coords (tile_id, emb_dim) to have coords (i0,
-    # j0, emb_dim), where `i0` and `j0` represent the index of the pixel in the
-    # original image at the center of each tile
-    i_img_tile, j_img_tile = tile_dataset.index_to_img_ij(da_emb.tile_id.values)
-    i_img_tile_center = (i_img_tile + 0.5 * tile_dataset.nxt).astype(int)
-    j_img_tile_center = (j_img_tile + 0.5 * tile_dataset.nyt).astype(int)
-    da_emb["i0"] = ("tile_id"), i_img_tile_center
-    da_emb["j0"] = ("tile_id"), j_img_tile_center
-    da_emb = da_emb.set_index(tile_id=("i0", "j0")).unstack("tile_id")
-
-    da_emb.attrs["tile_nx"] = tile_dataset.nxt
-    da_emb.attrs["tile_ny"] = tile_dataset.nyt
-
-    return da_emb
