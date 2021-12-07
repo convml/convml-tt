fb9bcdbe138a9a200b60f6b49b6384ab622a63e8
Leif Denby <leif@denby.eu>
2021-12-07 17:14:18 +0000

    create data.common module for common props (#55)

--- convml_tt/data/dataset.py
+++ convml_tt/data/dataset.py
@@ -11,9 +11,7 @@ import parse
 from PIL import Image
 from torch.utils.data.dataset import Dataset
 from torchvision import transforms as tv_transforms
-
-
-TILE_FILENAME_FORMAT = "{triplet_id:05d}_{tile_type}.png"
+from .common import TILE_IDENTIFIER_FORMAT
 
 
 def get_load_transforms():
@@ -38,14 +36,13 @@ class TileType(enum.Enum):
     DISTANT = 2
 
 
-def _find_tile_files(data_dir, stage):
+def _find_tile_files(data_dir, stage, ext="png"):
     # dictionary to hold lists with filepaths for each tile type
     file_paths = {tile_type: [] for tile_type in TileType}
 
-    ext = TILE_FILENAME_FORMAT.split(".")[-1]
     full_path = Path(data_dir) / stage
     for f_path in sorted(full_path.glob(f"*.{ext}"), key=lambda p: p.name):
-        file_info = parse.parse(TILE_FILENAME_FORMAT, f_path.name)
+        file_info = parse.parse(TILE_IDENTIFIER_FORMAT + f".{ext}", f_path.name)
         tile_name = file_info["tile_type"]
         try:
             tile_type = TileType[tile_name.upper()]
