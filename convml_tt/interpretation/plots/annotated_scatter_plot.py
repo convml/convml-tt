93f4b8599508625ce2aa09906bb01189b1333cd5
Leif Denby <leif@denby.eu>
2022-02-14 20:21:41 +0000

    switch to AnnotationBbox to make annotated scatter zoomable

--- convml_tt/interpretation/plots/annotated_scatter_plot.py
+++ convml_tt/interpretation/plots/annotated_scatter_plot.py
@@ -2,6 +2,7 @@ from collections import OrderedDict
 import enum
 
 import matplotlib.pyplot as plt
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 import numpy as np
 import seaborn as sns
 import xarray as xr
@@ -222,27 +223,37 @@ def annotated_scatter_plot(
         if zorder is not None:
             ax_zorder = zorder.sel(tile_id=tile_id)
             ax_kwargs["zorder"] = ax_zorder
-        ax1 = fig.add_axes([xp - 0.5 * size, yh - size * 0.5, size, size], **ax_kwargs)
-        ax1.set_aspect(1)
-        ax1.axison = False
+
+        img_idx = int(tile_id.values)
+        img = tile_dataset.get_image(index=img_idx)
+        oim = OffsetImage(img, zoom=size)
+        oim.image.axes = ax
+
+        ab = AnnotationBbox(
+            oim,
+            [x_, y_],
+            xybox=(0.0, 0.0),
+            xycoords="data",
+            boxcoords="offset points",
+            pad=0.0,
+            bboxprops=dict(color="none"),
+        )
+        ax.add_artist(ab)
 
         if hue is not None:
             label_text = hue.sel(tile_id=tile_id).item()
             if isinstance(label_text, xr.DataArray):
                 label_text = label_text.values
-            ax1.text(
+            ab.text(
                 0.1,
                 0.15,
                 label_text,
-                transform=ax1.transAxes,
+                transform=ab.transAxes,
                 bbox={"facecolor": "white", "alpha": 0.6, "pad": 2},
             )
 
-        img_idx = int(tile_id.values)
-        img = tile_dataset.get_image(index=img_idx)
-
         # img = Image.open(tiles_path/"{:05d}_anchor.png".format())
-        ax1.imshow(img)
+        # ax1.imshow(img)
 
     # ensure that adding the points doesn't change the axis limits since we
     # calculated the offset for the annotations
